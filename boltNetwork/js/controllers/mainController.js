7fd6c958695dc3d93eb095930914db708900dc5e
julie-j-code <jeannet.julie@gmail.com>
2020-05-06 08:01:15 +0200

    Angular JS codecademy Projets 1er BoltNetwork

---
+++ boltNetwork/js/controllers/mainController.js
@@ -0,0 +1,25 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.programs = [{
+  series: "Sherlock",
+  series_img: "img/sherlock.jpg",
+  genre: "Crime drama",
+  season: 3,
+  episode: "The Empty Hearse",
+  description: "Two years after his reported Reichenbach Fall demise, Sherlock, who has been cleared of all fraud charges against him, returns with Mycroft's help to a London under threat of terrorist attack. John has moved on and has a girlfriend, Mary Morstan. Sherlock enlists Molly to assist him, but when John is kidnapped by unknown assailants and is rescued by Sherlock and Mary, John returns to help find the terrorists and an underground plot to blow up the Houses of Parliament during an all night sitting on Guy Fawkes Night.",
+  datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
+},
+
+{
+  series: "Better Call Saul",
+  series_img: "img/better-call-saul.jpg",
+  genre: "Crime drama",
+  season: 5,
+  episode: "Bad Choice Road",
+  description: "The trials and tribulations of criminal lawyer Jimmy McGill in the time before he established his strip-mall law office in Albuquerque, New Mexico..",
+  datetime: new Date(2020, 03, 31, 21, 00, 00, 00)
+}
+]
+  
+
+  
+}]);
